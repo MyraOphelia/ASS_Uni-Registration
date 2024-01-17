419ba7c9222286ca4d0b419b1af932004b030fd4
burst-untidy <123620174+burst-untidy@users.noreply.github.com>
2024-01-17 23:04:06 +0800

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -1,12 +1,39 @@
+import javax.swing.*;
+
 public class Gui{
-    public static void main(String[] args){
-        
-        LoginApp loginApp = new LoginApp();
-
-        loginApp.setVisible(true);
-
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Gui().runGui());
     }
 
-   
+    public interface LoginListener {
+        void onLogin(String username, String userType);
+    }
+    
+
+    public void runGui() {
+        LoginApp loginApp = new LoginApp();
+
+        // Add a callback for successful student login
+        loginApp.setLoginCallback((username, userType) -> {
+            if ("Student".equals(userType)) {
+                openStudentPage(username);
+            }
+            else if ("Lecturer".equals(userType)){
+                openLecturerPage(username);
+            }
+        });
+
+        loginApp.setVisible(true);
+    }
+    
+
+    private void openStudentPage(String username) {
+        JFrame studentFrame = new StudentPage(username);
+        studentFrame.setVisible(true);
+    }
+
+    private void openLecturerPage(String username) {
+        JFrame lecturerFrame = new LecturerPage(username);
+        lecturerFrame.setVisible(true);
+    }
 }
